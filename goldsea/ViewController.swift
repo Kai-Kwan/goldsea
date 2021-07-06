549e3380d23eb343fb8326cdd36998e1d22f0f56
kai <aykaikwan@gmail.com>
2021-07-06 20:54:12 +0800

    Initial Commit

---
+++ goldsea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goldsea
+//
+//  Created by au yeung kai kwan on 6/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
