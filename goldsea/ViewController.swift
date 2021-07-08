c3825d165401c970d851dd4dc71734929cef9923
kai <aykaikwan@gmail.com>
2021-07-08 20:50:40 +0800

    ver. 0.1.0

--- goldsea/ViewController.swift
+++ goldsea/ViewController.swift
@@ -5,15 +5,26 @@
 //  Created by au yeung kai kwan on 6/7/2021.
 //
 
+
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+        
+        let myURL = URL(string:"https://goldsealand.com/a/app/index.html")
+        let myRequest = URLRequest(url: myURL!)
+        
+        webView.load(myRequest)
+    }}
